b338cb55c34118e10ecaad434ed636c95e0504a4
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-10-19 23:25:46 -0500

    added animation engine and did animations for login view controller

---
+++ BrainTeaser/AnimationEngine.swift
@@ -0,0 +1,63 @@
+//
+//  AnimationEngine.swift
+//  BrainTeaser
+//
+//  Created by Ronald Hernandez on 10/19/16.
+//  Copyright © 2016 Ronaldoh1. All rights reserved.
+//
+
+import pop
+import UIKit
+
+class AnimationEngine {
+    
+    class var offScreenRightPosition: CGPoint {
+        return CGPointMake(UIScreen.mainScreen().bounds.width, CGRectGetMidY(UIScreen.mainScreen().bounds))
+    }
+    
+    class var offScreenLeftPoint: CGPoint {
+        return CGPointMake(-UIScreen.mainScreen().bounds.width, CGRectGetMidY(UIScreen.mainScreen().bounds))
+    }
+    
+    class var screenCenterPosition: CGPoint {
+        return CGPointMake(CGRectGetMidX(UIScreen.mainScreen().bounds), CGRectGetMidY(UIScreen.mainScreen().bounds))
+    }
+    
+    var originalConstants = [CGFloat]()
+    var constraints = [NSLayoutConstraint]()
+    
+    init(constraints: [NSLayoutConstraint]) {
+        for constraint in constraints {
+            originalConstants.append(constraint.constant)
+            constraint.constant = AnimationEngine.offScreenRightPosition.x
+        }
+        
+        self.constraints = constraints
+    }
+    
+    func animateOnScreen(delay: Int) {
+        
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(Double(delay) * Double(NSEC_PER_SEC)))
+        
+        dispatch_after(time, dispatch_get_main_queue()) {
+           
+            var index = 0
+            repeat {
+                let moveAnimation = POPSpringAnimation(propertyNamed: kPOPLayoutConstraintConstant)
+                moveAnimation.toValue = self.originalConstants[index]
+                moveAnimation.springBounciness = 12
+                moveAnimation.springSpeed = 12
+                
+                if index > 0 {
+                    moveAnimation.dynamicsFriction += 20 + CGFloat(index)
+                }
+                
+                let constraint = self.constraints[index]
+                constraint.pop_addAnimation(moveAnimation, forKey: "moveOnScreen")
+                index += 1
+                
+            } while (index < self.constraints.count)
+        }
+    }
+    
+}
--- BrainTeaser/LoginVC.swift
+++ BrainTeaser/LoginVC.swift
@@ -11,15 +11,23 @@ import pop
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var emailConstraint: NSLayoutConstraint!
+    @IBOutlet weak var passwordConstraint: NSLayoutConstraint!
+    @IBOutlet weak var loginButtonConstraint: NSLayoutConstraint!
+    
+    var animationEngine: AnimationEngine!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.animationEngine = AnimationEngine(constraints: [emailConstraint, passwordConstraint, loginButtonConstraint])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.animationEngine.animateOnScreen(1)
     }
+  
 
 
 }
